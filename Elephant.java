d353e0326a6827c1dd420d148b3611de4b0dd42e
iUwUc <130993642+fewudi@users.noreply.github.com>
2023-05-17 20:46:25 -0400

    added sound and animation

--- Elephant.java
+++ Elephant.java
@@ -12,6 +12,47 @@ public class Elephant extends Actor
      * Act - do whatever the Elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    //import sound 
+    GreenfootSound soundEffect = new GreenfootSound("elephantcub.mp3");
+    
+    //image 
+    GreenfootImage[]idleRight = new GreenfootImage[8];
+    GreenfootImage[]idleLeft = new GreenfootImage[8];
+    
+    //initial direction of elephant is facing 
+    String facing = "right";
+    
+    
+    /*
+     * image constructor - The code that gets run one time when object is created 
+     */
+    public Elephant(){
+        //read images in to idle arraylist
+        for(int i = 0; i < idleRight.length; i++){
+            idleRight[i] = new GreenfootImage("images/elephant_idle/idle"+ i + ".png");
+            idleRight[i].scale(100, 100);
+        }
+        
+        for(int i = 0; i < idleLeft.length; i++){
+            idleLeft[i] = new GreenfootImage("images/elephant_idle/idle"+ i + ".png");
+            idleLeft[i] = idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);
+        }
+        
+        setImage(idleRight[0]);
+    }
+    
+    /*
+     * Animated the Elephant 
+     */
+    int imageIndex = 0;
+    public void animateElephant(){
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
+    
+    
     public void act()
     {
         // Add your action code here.
@@ -24,6 +65,9 @@ public class Elephant extends Actor
         
         // Remove apple if elephant eats it
         eat();
+        
+        //Animate the elephant
+        animateElephant();
     }
     
     // eat the apple and spwan new appple updating the label 
@@ -33,6 +77,7 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld)getWorld();
             world.createApple();
             world.increaseScore();
+            soundEffect.play();
         }
     }
 }
