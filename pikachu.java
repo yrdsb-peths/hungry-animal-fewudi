5952f174b65d16125839ad738074471eabed95b2
iUwUc <130993642+fewudi@users.noreply.github.com>
2023-05-25 09:02:29 -0400

    Create pikachu.java

---
+++ pikachu.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class pikachu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class pikachu extends Actor
+{
+    GreenfootImage idle = new GreenfootImage(
+    
+    
+    
+    
+    /**
+     * Act - do whatever the pikachu wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        GreenfootImage [] idleRight = new GreenfootImage[4];
+        GreenfootImage [] idleLeft = new GreenfootImage[4];
+        
+        String facing = "right";
+        SimpleTimer animationTimer = new SimpleTimer();
+        
+        
+    }
+}
